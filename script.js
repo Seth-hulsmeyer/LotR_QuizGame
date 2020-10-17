36aae4404ab3e416426aafb075752f8228fbad5c
Seth-hulsmeyer <seth.hulsmeyer@hotmail.com>
2020-10-17 09:51:55 -0600

    added file types to all images from the assets folder

--- script.js
+++ script.js
@@ -25,47 +25,47 @@ var questionArray = [
     answer: "Dwarves"},
 
     {question: "What kind of creatures are the spawn of Ungoliant?", 
-    imgSource: "./assets/LOTR2", 
+    imgSource: "./assets/LOTR2.jpg", 
     choices: ["Hill Giants", "Giant Spiders", "Uruk-hai", "Balrogs"], 
     answer: "Giant Spiders"},
 
     {question: "What do the Elves call the Hobbits?", 
-    imgSource: "./assets/LOTR3", 
+    imgSource: "./assets/LOTR3.png", 
     choices: ["The Periannath", "The Mellon", "The Uruloki", "The Onodrim"], 
     answer: "The Periannath"},
 
     {question: "What are the Palantiri, and how many are there?",
-     imgSource: "./assets/LOTR4", 
+     imgSource: "./assets/LOTR4.jpg", 
      choices: ["Seven Warning-Beacons", "Nine Rings of Power given to Men", "Three Jewels created by Feanor", "Seven Seeing-Stones"], 
      answer: "Seven Seeing-stones"},
 
     {question: "Which character said 'Nine companions. So be it. You shall be the fellowship of the ring.'?", 
-    imgSource: "./assets/LOTR5", 
+    imgSource: "./assets/LOTR5.jpg", 
     choices: ["Gandalf", "Arwen", "Elrond", "Tom Bombadil"], 
     answer: "Elrond"},
 
     {question: "After Sauron, who geld the One Ring?", 
-    imgSource: "./assets/LOTR6", 
+    imgSource: "./assets/LOTR6.jpg", 
     choices: ["It fell into a river and was lost", "Isildur", "Elrond", "Gollum"], 
     answer: "Isildur"},
 
     {question: "How many years did Frodo have the ring before he left the Shire?", 
-    imgSource: "./assets/LOTR7", 
+    imgSource: "./assets/LOTR7.jpg", 
     choices: ["17", "29", "8", "21"], 
     answer: "17"},
 
     {question: "Where is Legolas from?", 
-    imgSource: "./assets/LOTR8", 
+    imgSource: "./assets/LOTR8.jpg", 
     choices: ["Lothlorian", "Rivendell", "Mirkwood", "Fangorn Forest"], 
     answer: "Lothlorian"},
 
     {question: "On Weathertop, with what type of blade was Frodo stabbed?", 
-    imgSource: "./assets/LOTR9", 
+    imgSource: "./assets/LOTR9.jpg", 
     choices: ["Mithril", "Melkor", "Morgul", "Mithrandir"], 
     answer: "Morgul"},
 
     {question: "What is the name of the sword held in this picture?", 
-    imgSource: "./assets/LOTR10", 
+    imgSource: "./assets/LOTR10.jpg", 
     choices: ["Sting", "Narsil", "Glamdring", "Anduril"], 
     answer: "Anduril"},
 ];
@@ -78,7 +78,6 @@ function displayQuestion(){
 
     //NEED TO GET IMAGES TO APPEND
     var imgEL = document.createElement("img").setAttribute("src", questionArray[currentQuestion].imgSource);
-    // imgEl.setAttribute("src", questionArray[currentQuestion].imgSource);
     mainEl.append(imgEL);
 
     for(i = 0; i < questionArray[currentQuestion].choices.length; i++){
