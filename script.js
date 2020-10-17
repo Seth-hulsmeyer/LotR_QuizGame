99cc1947877df56fb0a1585fd21592f65a20a891
Seth-hulsmeyer <seth.hulsmeyer@hotmail.com>
2020-10-17 17:50:27 -0600

    added endscreen function. need to fix some style elements and score tracking bugs

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
+// Starter page
+document.getElementById("questionContainer").style.visibility = 'hidden';
+document.getElementById("endPage").style.visibility = 'hidden';
+var start = document.getElementById("startBtn");
+start.addEventListener("click", function(){
+  document.getElementById("startPage").style.visibility = 'hidden';
+  startPage.innerHTML = "";
+  document.getElementById("questionContainer").style.visibility = 'visible';
+  displayQuestion();
+  setTime();
+})
+
+
 // time interval variable 
-var timer = document.querySelector("time");
-var secondsLeft = 250;
+var timer = document.getElementById("time");
+var secondsLeft = 2000;
 function setTime() {
   var timerInterval = setInterval(function() {
-    secondsLeft--;
-    // timer.textContent = secondsLeft;
+    timer.textContent = "Timer: " + secondsLeft;
 
-    if(secondsLeft === 0) {
+    secondsLeft--;
+    
+    if(secondsLeft <= 0 || currentQuestion === questionArray.length) {
       clearInterval(timerInterval);
-      alert("You're out of time! Please try again.");
-        return;
+      endScreen();
     }
 
   }, 1000);
@@ -78,37 +91,59 @@ function displayQuestion(){
     mainElement.append(questionText);
 
     //NEED TO GET IMAGES TO APPEND
-    var imageEL = document.createElement("img")
+    var imageEL = document.createElement("img");
     imageEL.setAttribute("src", questionArray[currentQuestion].imgSource);
     mainElement.append(imageEL);
-
+    // line break for style and order
     var lineBreak = document.createElement("br")
     mainElement.append(lineBreak);
 
     for(i = 0; i < questionArray[currentQuestion].choices.length; i++){
         var choicesBtn = document.createElement("button");
         choicesBtn.innerText = questionArray[currentQuestion].choices[i];
-        choicesBtn.className = "btn";
+        // choicesBtn.className = "btn";
         choicesBtn.attributes = questionArray[currentQuestion].answer;
-        mainElement.append(choicesBtn);
-}}
+        mainElement.append(choicesBtn);   
+    }
 
-// //NEED TO GET QUESTIONS TO CYCLE THROUGH
-setTime();
-displayQuestion();
+
+}
+
+
+function checkAnswer(event){
+  if(event === questionArray[currentQuestion].answer){
+    secondsLeft += 10;
+  }else{
+    secondsLeft -= 10;
+  }
+}
+
 //click handler to cycle through questions after any button is clicked
 document.getElementById("questionContainer").addEventListener("click", function(event){
   event.preventDefault();
     //if clicked choice !== answer subtract seconds from timer, and cycle to next question
     if(event.target.matches("button")){
+      // var choice = event.target("button");
+     checkAnswer(event);
     currentQuestion++;
     displayQuestion();
     };
-    //go through the object to find the attribute
   
-      
-
-
+    //go through the object to find the attribute
   
     //console log event
 })
+
+function endScreen(){
+  if (secondsLeft <= 0){
+    alert("You ran out of time! Try again.")
+    return displayQuestion();
+  } else{
+    var score = secondsLeft;
+    console.log(score);
+    document.getElementById("time").style.visibility='hidden';
+    document.getElementById("questionContainer").style.visibility='hidden';
+    document.getElementById("endPage").style.visibility = 'visible';
+    score.getElementById("scoreDisplay").append(score);
+  }
+}
